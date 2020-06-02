17a0333fa3118b0369a2017a056a10d4631bc0e0
alby <alby@disroot.org>
2020-06-02 23:28:05 +0530

    sdcv output order

--- words.sh
+++ words.sh
@@ -4,6 +4,8 @@ while true
 do
     w=$(cat /usr/share/dict/british-english | dmenu)
     [ $1 = 3 ] && flite -t "$w" -voice slt &
-    [ -n "$w" ] && sdcv $w -n | dmenu -l 20
+    [ -n "$w" ] &&
+    sdcv -u 'WordNet' -u 'Moby Thesaurus II' $w -ne |
+    dmenu -l 20
     [ $? = 1 ] && break
 done
